dc31a2582cd345e01295cbfb5e08221c0b18d97a
Mario Zupan <mario@zupzup.org>
2022-02-04 18:03:57 +0100

    step add blogpost blogpostcomment storagemaps

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -138,6 +138,10 @@ parameter_types! {
 	pub BlockLength: frame_system::limits::BlockLength = frame_system::limits::BlockLength
 		::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
 	pub const SS58Prefix: u8 = 42;
+	pub const BlogPostMinBytes: u32 = 64;
+	pub const BlogPostMaxBytes: u32 = 4096;
+	pub const BlogPostCommentMinBytes: u32 = 64;
+	pub const BlogPostCommentMaxBytes: u32 = 1024;
 }
 
 // Configure FRAME pallets to include in runtime.
@@ -278,6 +282,10 @@ impl pallet_sudo::Config for Runtime {
 impl pallet_blogchain::Config for Runtime {
 	type Event = Event;
 	type Currency = Balances;
+	type BlogPostMinBytes = BlogPostMinBytes;
+	type BlogPostMaxBytes = BlogPostMaxBytes;
+	type BlogPostCommentMinBytes = BlogPostCommentMinBytes;
+	type BlogPostCommentMaxBytes = BlogPostCommentMaxBytes;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
