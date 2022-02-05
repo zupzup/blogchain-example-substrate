969a3247fdcd0647afb83dbf237a9d28b5af85dd
Mario Zupan <mario@zupzup.org>
2022-02-05 20:49:57 +0100

    step comment and tip

--- README.md
+++ README.md
@@ -3,18 +3,30 @@
 
 An example for a Substrate-Node-Template backed custom blockchain
 
-TODO keep setup and run and delete rest
 
-### Single-Node Development Chain
+Compile with:
 
-This command will start the single-node development chain with non-persistent state:
+```bash
+cargo build --release
+```
+
+Start with:
 
 ```bash
 ./target/release/node-template --dev
 ```
 
-Purge the development chain's state:
+Then you can either use [https://github.com/substrate-developer-hub/substrate-front-end-template](https://github.com/substrate-developer-hub/substrate-front-end-template) to start up a frontend to connect to the blockchain, or you can use [https://polkadot.js.org/apps/#/explorer](https://polkadot.js.org/apps/#/explorer) in `Development` mode using `Local Node`
 
-```bash
-./target/release/node-template purge-chain --dev
-```
+## Functionality
+
+There is a custom pallet called `blogchain` implemented, which features a simplistic blogging service.
+
+In terms of extrinsics (functions you can use to interact with the blockchain), you can do three things:
+
+* Create a new blog post (blogchain -> `create_blog_post`) - enter an arbitrary string between 64 and 4096 bytes as the content of your post
+    * Costs: 10000
+* Comment on an existing blog post (blogchain -> `create_blog_post_comment`) - enter a blog post id and an arbitrary string between 64 and 1024 bytes - this comment will be saved in a list on the blog post
+    * Costs: 5000
+* Tip the blog post author (blogchain -> `tip_blog_post`) - enter a blog post id and an amount to send
+    * Costs: 500 (+amount you wish to send, obviously)
--- pallets/blogchain/src/lib.rs
+++ pallets/blogchain/src/lib.rs
@@ -8,7 +8,11 @@ pub use pallet::*;
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::{
-		inherent::Vec, pallet_prelude::*, sp_runtime::traits::Hash, traits::Currency,
+		inherent::Vec,
+		pallet_prelude::*,
+		sp_runtime::traits::Hash,
+		traits::{tokens::ExistenceRequirement, Currency},
+		transactional,
 	};
 	use frame_system::pallet_prelude::*;
 
@@ -82,6 +86,8 @@ pub mod pallet {
 		BlogPostTooManyBytes,
 		BlogPostCommentNotEnoughBytes,
 		BlogPostCommentTooManyBytes,
+		BlogPostNotFound,
+		TipperIsAuthor,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -90,6 +96,7 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(10000)]
+		#[transactional]
 		pub fn create_blog_post(origin: OriginFor<T>, content: Vec<u8>) -> DispatchResult {
 			let author = ensure_signed(origin)?;
 
@@ -115,8 +122,69 @@ pub mod pallet {
 
 			<BlogPosts<T>>::insert(blog_post_id, blog_post);
 
+			let comments_vec: Vec<BlogPostComment<T>> = Vec::new();
+			<BlogPostComments<T>>::insert(blog_post_id, comments_vec);
+
 			Self::deposit_event(Event::BlogPostCreated(content, author, blog_post_id));
 
+			Ok(())
+		}
+
+		#[pallet::weight(5000)]
+		pub fn create_blog_post_comment(
+			origin: OriginFor<T>,
+			content: Vec<u8>,
+			blog_post_id: T::Hash,
+		) -> DispatchResult {
+			let comment_author = ensure_signed(origin)?;
+
+			ensure!(
+				(content.len() as u32) > T::BlogPostMinBytes::get(),
+				<Error<T>>::BlogPostCommentNotEnoughBytes
+			);
+
+			ensure!(
+				(content.len() as u32) < T::BlogPostMaxBytes::get(),
+				<Error<T>>::BlogPostCommentTooManyBytes
+			);
+
+			let blog_post_comment = BlogPostComment {
+				author: comment_author.clone(),
+				content: content.clone(),
+				blog_post_id: blog_post_id.clone(),
+			};
+
+			<BlogPostComments<T>>::mutate(blog_post_id, |comments| match comments {
+				None => Err(()),
+				Some(vec) => {
+					vec.push(blog_post_comment);
+					Ok(())
+				},
+			})
+			.map_err(|_| <Error<T>>::BlogPostNotFound)?;
+
+			Ok(())
+		}
+
+		#[pallet::weight(500)]
+		pub fn tip_blog_post(
+			origin: OriginFor<T>,
+			blog_post_id: T::Hash,
+			amount: <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance,
+		) -> DispatchResult {
+			let tipper = ensure_signed(origin)?;
+
+			let blog_post = Self::blog_posts(&blog_post_id).ok_or(<Error<T>>::BlogPostNotFound)?;
+			let blog_post_author = blog_post.author;
+
+			ensure!(tipper != blog_post_author, <Error<T>>::TipperIsAuthor);
+
+			T::Currency::transfer(
+				&tipper,
+				&blog_post_author,
+				amount,
+				ExistenceRequirement::KeepAlive,
+			)?;
 			Ok(())
 		}
 	}
